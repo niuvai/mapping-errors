cde0bed2855183f681e8bbb874b2b91cd8c5aa58
Luc Baudouin <luc.baudouin@cirad.fr>
2016-02-04 11:15:05 +0100

    encore un changement de commentaire

--- sources/distance.R
+++ sources/distance.R
@@ -1,4 +1,5 @@
 distance<-function(pop,i,j){
   # Computes recombination distances. This new version accepts NAs
+  # à nouveau..
   sum(pop[,i]!=pop[,j],na.rm=TRUE)/sum(!(is.na(pop[,i])|is.na(pop[,j])))
 }
