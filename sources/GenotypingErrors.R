f4c9f0c24c849133e5a06cdc994bd57b9b16740f
Luc Baudouin <luc.baudouin@cirad.fr>
2016-02-04 11:07:44 +0100

    j'ai changé le commentaire

--- sources/GenotypingErrors.R
+++ sources/GenotypingErrors.R
@@ -1,5 +1,5 @@
 GenotypingErrors<-function(pop,genotErr){
-  # genotyping errors
+  # genotyping errors une vieille version
   
   err<-matrix(sample(g,nrow(pop)*ncol(pop),replace=TRUE,prob=c(genotErr,1-genotErr)),nrow=nrow(pop))
   pop!=err # TT->F, TF-<T, FT->T and FF->F
